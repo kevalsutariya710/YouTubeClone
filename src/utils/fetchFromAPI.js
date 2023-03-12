ab590021c1c45e2b60076e03ab9255d63cff65f6
Keval Sutariya <100850842+kevalsutariya710@users.noreply.github.com>
2023-03-12 13:04:04 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-
-
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
@@ -16,6 +14,5 @@ const options = {
 
 export const fetchFromAPI = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
     return data;
 };
